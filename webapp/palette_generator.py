b3a298dcb797c2acffff891d5ded425403071062
Z3R0C1PH3R <Z3R0C1PH3R@protonmail.com>
2024-07-09 19:52:48 +0530

    Add -ve ranges

--- webapp/palette_generator.py
+++ webapp/palette_generator.py
@@ -1,14 +1,22 @@
 import random
 
+
+def sign(n):
+    if n == 0:
+        return 0
+    if n > 0:
+        return 1
+    return -1
+
 def rgb_palette(size = 1):
     return [(random.random(), random.random(), random.random()) for _ in range(size)]
 
 def hls_palette(size = 1, h_spread=1, l_spread=0, s_spread=0):
     h = random.random()
     dh = h_spread/size
-    s = random.random()*(1-s_spread-0.1)+0.1 if s_spread else random.random()/2+0.3
+    s = 1 + sign(s_spread)*(random.random()*(1-s_spread-0.1)+0.1) if s_spread else random.random()/2+0.3
     ds = s_spread/size
-    l = random.random()*(1-l_spread-0.1)+0.1 if l_spread else random.random()/2+0.3
+    l = 1 + sign(l_spread)*(random.random()*(1-l_spread-0.1)+0.1) if l_spread else random.random()/2+0.3
     dl = l_spread/size
-    return [(((h+i*dh)%1)*360, l+i*dl, s+i*ds) for i in range(size)]
+    return [(((h+i*dh)%1)*360, (l+i*dl)%1, (s+i*ds)%1) for i in range(size)]
 
