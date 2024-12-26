daf58c7e8a3e699272cdf2ad331472fdfde78f95
Z3R0C1PH3R <Z3R0C1PH3R@protonmail.com>
2024-12-26 18:51:55 +0530

    Improve Palette generator

--- webapp/palette_generator.py
+++ webapp/palette_generator.py
@@ -14,9 +14,9 @@ def rgb_palette(size = 1):
 def hls_palette(size = 1, h_spread=1, l_spread=0, s_spread=0):
     h = random.random()
     dh = h_spread/size
-    s = 1 + sign(s_spread)*(random.random()*(1-s_spread-0.1)+0.1) if s_spread else random.random()/2+0.3
+    s = 1 + sign(s_spread)*(random.random()*(1-abs(s_spread)-0.1)+0.1)+(1-sign(s_spread))/2 if s_spread else random.random()/2+0.3
     ds = s_spread/size
-    l = 1 + sign(l_spread)*(random.random()*(1-l_spread-0.1)+0.1) if l_spread else random.random()/2+0.3
+    l = 1 + sign(l_spread)*(random.random()*(1-abs(l_spread)-0.1)+0.1)+(1-sign(l_spread))/2 if l_spread else random.random()/2+0.3
     dl = l_spread/size
     return [(((h+i*dh)%1)*360, (l+i*dl)%1, (s+i*ds)%1) for i in range(size)]
 
